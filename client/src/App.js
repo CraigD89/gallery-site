1dd258d50ec25d4f49b4271520c188b125e8b109
Craig D <dupuiscraig@gmail.com>
2018-03-26 12:42:45 -0500

    builds out front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import Home from "./pages/Home";
-// import Artists from "./pages/Artists";
-// import Info from "./pages/Info";
 import Nav from "./components/Nav";
+// import Wrapper from "./components/Wrapper";
+import Home from "./pages/Home";
+import Artists from "./pages/Artists";
+import Contact from "./pages/Contact";
+
+
 
 const App = () => (
   <MuiThemeProvider>
     <Router>
       <div>
         <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          {/* <Route exact path="/artists" component={Artists} /> */}
-          {/* <Route exact path="/artists/:id" component={ArtistsDetail} /> */}
-          {/* <Route exact path="/info" component={Info} /> */}
-          {/* <Route component={NoMatch} /> */}
-        </Switch>
+        {/* <Wrapper> */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/artists" component={Artists} />
+            {/* <Route exact path="/artists/:id" component={ArtistsDetail} /> */}
+            <Route exact path="/contact" component={Contact} />
+            {/* <Route component={NoMatch} /> */}
+          </Switch>
+        {/* </Wrapper> */}
       </div>
     </Router>
   </MuiThemeProvider>
